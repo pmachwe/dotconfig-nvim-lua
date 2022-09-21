aed311c8a94b955d96087aeabb2c49e418d39608
Parikshit Machwe <pmachwe@gmail.com>
2022-09-21 21:20:28 +0530

    set theme based on env

---
+++ lua/user/functions.lua
@@ -0,0 +1,15 @@
+-- Set theme based on env variable
+-- to light/dark versions
+
+set_theme = function()
+    local cs
+    if (os.getenv("MY_BACKGROUND") == "dark") then
+        cs = "nordfox"
+    else
+        cs = "dawnfox"
+    end
+
+    print(vim.inspect("Setting colorscheme to " .. cs))
+
+    vim.cmd("colorscheme " .. cs)
+end
