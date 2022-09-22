da493871e9afdd5b8cb650279b8e5b1a0880b420
Parikshit Machwe <pmachwe@gmail.com>
2022-09-22 17:08:13 +0530

    Trying which-key, need to register personal mappings

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -70,5 +70,15 @@ return require('packer').startup(function(use)
     "AckslD/nvim-neoclip.lua",
     config = function() require('neoclip').setup() end,
   }
+  use {
+      "folke/which-key.nvim",
+      config = function()
+          require("which-key").setup {
+              -- your configuration comes here
+              -- or leave it empty to use the default settings
+              -- refer to the configuration section below
+          }
+      end
+  }
 
 end)
