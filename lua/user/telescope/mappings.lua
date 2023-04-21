c8536097528c72c2ff0c0239d4dce50f4249e88e
Parikshit Machwe <pmachwe@gmail.com>
2023-04-21 23:10:01 +0530

    fixing syntax error in telescope mappings

--- lua/user/telescope/mappings.lua
+++ lua/user/telescope/mappings.lua
@@ -1,35 +1,35 @@
 local opts = { noremap = true, silent = true }
 
 -- Telescope basics
-vim.api.nvim_set_keymap("n", "<leader>,", "<cmd>Telescope buffers show_all_buffers=true<cr>", {noremap = true, silent = true, desc = "Switch Buffer"}),
-vim.api.nvim_set_keymap("n", "<leader>:", "<cmd>Telescope command_history<cr>", {noremap = true, silent = true, desc = "Command History"}),
+vim.api.nvim_set_keymap("n", "<leader>,", "<cmd>Telescope buffers show_all_buffers=true<cr>", {noremap = true, silent = true, desc = "Switch Buffer"})
+vim.api.nvim_set_keymap("n", "<leader>:", "<cmd>Telescope command_history<cr>", {noremap = true, silent = true, desc = "Command History"})
 -- find
-vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", {noremap = true, silent = true, desc = "Buffers"}),
-vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", {noremap = true, silent = true, desc = "Find Files (root dir)"}),
-vim.api.nvim_set_keymap("n", "<leader>fe", "<cmd>lua require('telescope.builtin').find_files({cwd = vim.fn.expand('%:p:h')))<cr>", {noremap = true, silent = true, desc = "Find Files (cwd)"}),
-vim.api.nvim_set_keymap("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", {noremap = true, silent = true, desc = "Recent"}),
-vim.api.nvim_set_keymap("n", '<Leader>fi', "<cmd>lua require('telescope.builtin').find_files({cwd = '~/.config/nvim'))<CR>", {noremap = true, silent = true, desc = "Find Files Nvim Config"}),
+vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", {noremap = true, silent = true, desc = "Buffers"})
+vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", {noremap = true, silent = true, desc = "Find Files (root dir)"})
+vim.api.nvim_set_keymap("n", "<leader>fe", "<cmd>lua require('telescope.builtin').find_files({cwd = vim.fn.expand('%:p:h')))<cr>", {noremap = true, silent = true, desc = "Find Files (cwd)"})
+vim.api.nvim_set_keymap("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", {noremap = true, silent = true, desc = "Recent"})
+vim.api.nvim_set_keymap("n", '<Leader>fi', "<cmd>lua require('telescope.builtin').find_files({cwd = '~/.config/nvim'))<CR>", {noremap = true, silent = true, desc = "Find Files Nvim Config"})
 -- git
-vim.api.nvim_set_keymap("n", "<leader>gc", "<cmd>Telescope git_commits<CR>", {noremap = true, silent = true, desc = "commits"}),
-vim.api.nvim_set_keymap("n", "<leader>gs", "<cmd>Telescope git_status<CR>", {noremap = true, silent = true, desc = "status"}),
+vim.api.nvim_set_keymap("n", "<leader>gc", "<cmd>Telescope git_commits<CR>", {noremap = true, silent = true, desc = "commits"})
+vim.api.nvim_set_keymap("n", "<leader>gs", "<cmd>Telescope git_status<CR>", {noremap = true, silent = true, desc = "status"})
 -- search
-vim.api.nvim_set_keymap("n", "<leader>sa", "<cmd>Telescope autocommands<cr>", {noremap = true, silent = true, desc = "Auto Commands"}),
-vim.api.nvim_set_keymap("n", "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", {noremap = true, silent = true, desc = "Buffer"}),
-vim.api.nvim_set_keymap("n", "<leader>sc", "<cmd>Telescope command_history<cr>", {noremap = true, silent = true, desc = "Command History"}),
-vim.api.nvim_set_keymap("n", "<leader>sC", "<cmd>Telescope commands<cr>", {noremap = true, silent = true, desc = "Commands"}),
-vim.api.nvim_set_keymap("n", "<leader>sd", "<cmd>Telescope diagnostics<cr>", {noremap = true, silent = true, desc = "Diagnostics"}),
-vim.api.nvim_set_keymap("n", "<leader>sg", "<cmd>Telescope live_grep<cr>", {noremap = true, silent = true, desc = "Grep (root dir)"}),
-vim.api.nvim_set_keymap("n", "<leader>se", "<cmd>lua require('telescope.builtin').live_grep({cwd = vim.fn.expand('%:p:h')))<cr>", {noremap = true, silent = true, desc = "Grep (cwd)"}),
-vim.api.nvim_set_keymap("n", "<leader>/", "<cmd>lua require('telescope.builtin').live_grep({cwd = vim.fn.expand('%:p:h')))<cr>", {noremap = true, silent = true, desc = "Grep (cwd)"}),
-vim.api.nvim_set_keymap("n", "<leader>sh", "<cmd>Telescope help_tags<cr>", {noremap = true, silent = true, desc = "Help Pages"}),
-vim.api.nvim_set_keymap("n", "<leader>sH", "<cmd>Telescope highlights<cr>", {noremap = true, silent = true, desc = "Search Highlight Groups"}),
-vim.api.nvim_set_keymap("n", "<leader>sk", "<cmd>Telescope keymaps<cr>", {noremap = true, silent = true, desc = "Key Maps"}),
-vim.api.nvim_set_keymap("n", "<leader>sM", "<cmd>Telescope man_pages<cr>", {noremap = true, silent = true, desc = "Man Pages"}),
-vim.api.nvim_set_keymap("n", "<leader>sm", "<cmd>Telescope marks<cr>", {noremap = true, silent = true, desc = "Jump to Mark"}),
-vim.api.nvim_set_keymap("n", "<leader>so", "<cmd>Telescope vim_options<cr>", {noremap = true, silent = true, desc = "Options"}),
-vim.api.nvim_set_keymap("n", "<leader>sR", "<cmd>Telescope resume<cr>", {noremap = true, silent = true, desc = "Resume"}),
-vim.api.nvim_set_keymap("n", "<leader>r", "<cmd>Telescope resume<cr>", {noremap = true, silent = true, desc = "Resume"}),
-vim.api.nvim_set_keymap("n", "<leader>t", "<cmd>Telescope treesitter<cr>", {noremap = true, silent = true, desc = "Treesitter symbols"}),
+vim.api.nvim_set_keymap("n", "<leader>sa", "<cmd>Telescope autocommands<cr>", {noremap = true, silent = true, desc = "Auto Commands"})
+vim.api.nvim_set_keymap("n", "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", {noremap = true, silent = true, desc = "Buffer"})
+vim.api.nvim_set_keymap("n", "<leader>sc", "<cmd>Telescope command_history<cr>", {noremap = true, silent = true, desc = "Command History"})
+vim.api.nvim_set_keymap("n", "<leader>sC", "<cmd>Telescope commands<cr>", {noremap = true, silent = true, desc = "Commands"})
+vim.api.nvim_set_keymap("n", "<leader>sd", "<cmd>Telescope diagnostics<cr>", {noremap = true, silent = true, desc = "Diagnostics"})
+vim.api.nvim_set_keymap("n", "<leader>sg", "<cmd>Telescope live_grep<cr>", {noremap = true, silent = true, desc = "Grep (root dir)"})
+vim.api.nvim_set_keymap("n", "<leader>se", "<cmd>lua require('telescope.builtin').live_grep({cwd = vim.fn.expand('%:p:h')))<cr>", {noremap = true, silent = true, desc = "Grep (cwd)"})
+vim.api.nvim_set_keymap("n", "<leader>/", "<cmd>lua require('telescope.builtin').live_grep({cwd = vim.fn.expand('%:p:h')))<cr>", {noremap = true, silent = true, desc = "Grep (cwd)"})
+vim.api.nvim_set_keymap("n", "<leader>sh", "<cmd>Telescope help_tags<cr>", {noremap = true, silent = true, desc = "Help Pages"})
+vim.api.nvim_set_keymap("n", "<leader>sH", "<cmd>Telescope highlights<cr>", {noremap = true, silent = true, desc = "Search Highlight Groups"})
+vim.api.nvim_set_keymap("n", "<leader>sk", "<cmd>Telescope keymaps<cr>", {noremap = true, silent = true, desc = "Key Maps"})
+vim.api.nvim_set_keymap("n", "<leader>sM", "<cmd>Telescope man_pages<cr>", {noremap = true, silent = true, desc = "Man Pages"})
+vim.api.nvim_set_keymap("n", "<leader>sm", "<cmd>Telescope marks<cr>", {noremap = true, silent = true, desc = "Jump to Mark"})
+vim.api.nvim_set_keymap("n", "<leader>so", "<cmd>Telescope vim_options<cr>", {noremap = true, silent = true, desc = "Options"})
+vim.api.nvim_set_keymap("n", "<leader>sR", "<cmd>Telescope resume<cr>", {noremap = true, silent = true, desc = "Resume"})
+vim.api.nvim_set_keymap("n", "<leader>r", "<cmd>Telescope resume<cr>", {noremap = true, silent = true, desc = "Resume"})
+vim.api.nvim_set_keymap("n", "<leader>t", "<cmd>Telescope treesitter<cr>", {noremap = true, silent = true, desc = "Treesitter symbols"})
 
 -- Extension-specific
 vim.api.nvim_set_keymap('n', '<Leader>fr', ":Telescope neoclip<CR>", opts)
